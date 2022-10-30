4aa7aecb7a0fe4a73cd93f3ff73b2ef6aeb86604
Christos Talagozis <talagozis@hotmail.com>
2022-10-30 14:32:33 +0200

    Implement API models.

---
+++ Binance.Client/Models/Api/ExchangeInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Binance.Client.Models.Api
+{
+    public class ExchangeInfo
+    {
+        public string? timezone { get; set; }
+        public long serverTime { get; set; }
+        public IReadOnlyCollection<Symbol>? symbols { get; set; }
+
+        public class Symbol
+        {
+            public string? symbol { get; set; }
+            public string? baseAsset { get; set; }
+            public string? quoteAsset { get; set; }
+            public IReadOnlyCollection<Filter>? filters { get; set; }
+
+            public class Filter
+            {
+                public string? filterType { get; set; }
+                public decimal? minPrice { get; set; }
+                public decimal? maxPrice { get; set; }
+                public decimal? tickSize { get; set; }
+                public decimal? minQty { get; set; }
+                public decimal? maxQty { get; set; }
+                public decimal? stepSize { get; set; }
+                public decimal? minNotional { get; set; }
+                public bool? applyToMarket { get; set; }
+                public decimal? avgPriceMins { get; set; }
+            }
+        }
+    }
+}
---
+++ Binance.Client/Models/Api/OrderType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Text.Json.Serialization;
+
+namespace Binance.Client.Models.Api
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public enum OrderType
+    {
+        LIMIT = 1,
+        MARKET = 2,
+
+        //STOP_LOSS = 3,
+        //STOP_LOSS_LIMIT = 4,
+        //TAKE_PROFIT = 5,
+        //TAKE_PROFIT_LIMIT = 6,
+        //LIMIT_MAKER = 7
+    }
+}
